7fb4b73119ee2bf5bd82968b6e74a7ce71992ec4
the lizard <72849089+the-liz4rd@users.noreply.github.com>
2021-04-03 18:22:24 +0430

    Update Spider.py

--- Spider.py
+++ Spider.py
@@ -2,12 +2,42 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 import sys
+from termcolor import colored
+
 
 def banner():
-	print("Banner")
+	print("""
+
+
+
+██╗░░░░░██╗███████╗░█████╗░██████╗░██████╗░  ███╗░░░███╗░█████╗░██████╗░███████╗  
+██║░░░░░██║╚════██║██╔══██╗██╔══██╗██╔══██╗  ████╗░████║██╔══██╗██╔══██╗██╔════╝  
+██║░░░░░██║░░███╔═╝███████║██████╔╝██║░░██║  ██╔████╔██║███████║██║░░██║█████╗░░  
+██║░░░░░██║██╔══╝░░██╔══██║██╔══██╗██║░░██║  ██║╚██╔╝██║██╔══██║██║░░██║██╔══╝░░  
+███████╗██║███████╗██║░░██║██║░░██║██████╔╝  ██║░╚═╝░██║██║░░██║██████╔╝███████╗  
+╚══════╝╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝  
+
+░██╗░░░░░░░██╗███████╗██████╗░  ░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗██╗░░░░░███████╗██████╗░
+░██║░░██╗░░██║██╔════╝██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║██║░░░░░██╔════╝██╔══██╗
+░╚██╗████╗██╔╝█████╗░░██████╦╝  ██║░░╚═╝██████╔╝███████║░╚██╗████╗██╔╝██║░░░░░█████╗░░██████╔╝
+░░████╔═████║░██╔══╝░░██╔══██╗  ██║░░██╗██╔══██╗██╔══██║░░████╔═████║░██║░░░░░██╔══╝░░██╔══██╗
+░░╚██╔╝░╚██╔╝░███████╗██████╦╝  ╚█████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░███████╗███████╗██║░░██║
+░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝╚═╝░░╚═╝
+
+		""")
 
 def usage():
-	print("usage")
+	print("""
+
+		--single-domain                  only crawl one domain, crawl all domains
+		--output                 -o      outout file's name
+		--time-out               -t      set a time out for the requests
+
+		""")
+
+
+
+banner()
 
 
 args = sys.argv
@@ -15,7 +45,6 @@ args.remove(sys.argv[0])
 
 
 if len(args) < 1 :
-	banner()
 	usage()
 	exit()
 
@@ -25,20 +54,39 @@ SingleDomain = False
 urls = []
 domains = []
 
-
+OutputFileName = "output.txt"
+timeoutValue = ""
 
 n = 0
+
+
 for arg in args:
 	n = n + 1
 	if arg.startswith('-'):
 		if arg == "-o" or arg == "--output":
 			OutputFileName = args[n]
-		if arg == "--single-domain":
+		elif arg == "--single-domain":
 			SingleDomain = True
+		elif arg == "--timeout" or arg == "-t":
+			try :
+				timeoutValue = int(args[n])
+			except:
+				print("[!] ERROR: Please select a valid number for timeout value")
+				exit()
+
 	else:
-		urls.append(arg)
+		if OutputFileName:
+			if arg == OutputFileName:
+				pass
+		if timeoutValue:
+			if arg == timeoutValue:
+				pass
+		else:
+			urls.append(arg)
 
 
+with open(OutputFileName, 'w') as f :
+	pass
 
 
 
@@ -52,14 +100,14 @@ for url in urls:
 		if len(rdomain) == 1:
 			domains.append(rdomain[0])
 		elif len(rdomain) > 1:
-			print("[!] Unknown ERROR")
+			print(colored("[!] Unknown ERROR", 'red'))
 			exit()
 		elif len(rdomain) < 1 :
 			rdomain = re.findall(r'https:\/\/(.+)', url)
 			if len(rdomain) == 1:
 				domains.append(rdomain[0])
 			else:
-				print("[!] Unknown ERROR")
+				print(colored("[!] Unknown ERROR", 'red'))
 
 	elif url.startswith("http://"):
 		proto = "http://"
@@ -67,41 +115,50 @@ for url in urls:
 		if len(rdomain) == 1:
 			domains.append(rdomain[0])
 		elif len(rdomain) > 1:
-			print("[!] Unknown ERROR")
+			print(colored("[!] Unknown ERROR", 'red'))
 			exit()
 		elif len(rdomain) < 1 :
 			rdomain = re.findall(r'http:\/\/(.+)', url)
 			if len(rdomain) == 1:
 				domains.append(rdomain[0])
 			else :
-				print("[!] Unknown ERROR")
+				print(colored("[!] Unknown ERROR", 'red'))
+	else:
+		print(colored("[!] ERROR: please enter a valid URL(EX: https://example.net)", 'red'))
+		print(colored("[!] ERROR: only HTTP and HTTPS can be accepted", 'red'))
+
+
 
 
 if len(domains) > 1:
-	print("[!] ERROR: more than one domain found, but the '--single-domain' option is used")
+	print(colored("[!] ERROR: more than one domain found, but the '--single-domain' option is used", 'red'))
 elif len(domains) < 1:
-	print("[!] ERROR: no Domain was found, check the url and try again")
+	print(colored("[!] ERROR: no Domain was found, check the url and try again", 'red'))
 elif len(domains) == 1:
 	domain = domains[0]
 
 
+index = 0
 
-
-
-
-
+string = ""
 
 
 for url in urls:
-	print(f"[*] Sending request to {url}")
-	res1 = requests.get(url)
+	index = index + 1
+	print(colored(f"[*] Sending request to {url}", 'yellow'))
+	if timeoutValue :
+		res1 = requests.get(url = url, timeout = timeoutValue)
+	else :
+		res1 = requests.get(url)
 	if res1.status_code == 200:
-		print(f"[+] {url}: {res1.status_code}")
+		print(colored(f"[+] {url}: {res1.status_code}", 'green'))
 	else:
-		print(f"[-] {url}: {res1.status_code}")
-	print(f"[*] Looking for Urls in {url}")
+		print(colored(f"[-] {url}: {res1.status_code}", 'red'))
+	print(colored(f"[*] Looking for Urls in {url}", 'yellow'))
 	soup = bs(res1.text, "html.parser")
 	tags = soup.select("a[href]")
+	print(colored(f"[+] {len(tags)} urls was found in '{url}'", 'green'))
+	print(colored("--------------------------------------", 'blue'))
 	if SingleDomain :
 		for a in tags:
 			alink = a.attrs.get("href")
@@ -112,7 +169,39 @@ for url in urls:
 	else:
 		for a in tags:
 			alink = a.attrs.get("href")
-			urls.append(alink)
+			if alink.startswith("/"):
+				urls.append(proto + domain + alink)
+			elif alink.startswith(url) or re.findall(rf".+:\/\/.*{domain}", url) :
+				urls.append(alink)
+			else:
+				urls.append(alink)
+
+	urls = list(set(urls))
+	urls.remove(url)
+	string = string + url + "\n"
+
+	if index >= 20:
+		with open(OutputFileName, "a") as f :
+			f.write(string)
+			string = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
