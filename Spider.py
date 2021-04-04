a2fedace0a74c5d06d2ea851c5826441c9fea741
the lizard <72849089+the-liz4rd@users.noreply.github.com>
2021-04-04 20:31:23 +0430

    Update Spider.py

--- Spider.py
+++ Spider.py
@@ -85,12 +85,23 @@ for arg in args:
 			urls.append(arg)
 
 
+if len(urls) > 1:
+	print("[!] ERROR: more than one url was given")
+	exit()
+elif len(urls) < 1:
+	print("[!] Please specify a URL")
+	exit()
+else:
+	mainURL = urls[0]
+
+
 with open(OutputFileName, 'w') as f :
 	pass
 
-
+"""
 
 proto = ''
+
 
 
 for url in urls:
@@ -128,14 +139,23 @@ for url in urls:
 		print(colored("[!] ERROR: only HTTP and HTTPS can be accepted", 'red'))
 
 
-
-
 if len(domains) > 1:
 	print(colored("[!] ERROR: more than one domain found, but the '--single-domain' option is used", 'red'))
 elif len(domains) < 1:
 	print(colored("[!] ERROR: no Domain was found, check the url and try again", 'red'))
 elif len(domains) == 1:
 	domain = domains[0]
+"""
+
+
+
+tmpRes = re.findall(r"(.+):\/\/(.+)", mainURL)
+
+proto = tmpRes[0][0]
+domain = tmpRes[0][1]
+
+
+
 
 
 index = 0
@@ -146,36 +166,40 @@ string = ""
 for url in urls:
 	index = index + 1
 	print(colored(f"[*] Sending request to {url}", 'yellow'))
-	if timeoutValue :
-		res1 = requests.get(url = url, timeout = timeoutValue)
-	else :
-		res1 = requests.get(url)
-	if res1.status_code == 200:
-		print(colored(f"[+] {url}: {res1.status_code}", 'green'))
-	else:
-		print(colored(f"[-] {url}: {res1.status_code}", 'red'))
-	print(colored(f"[*] Looking for Urls in {url}", 'yellow'))
-	soup = bs(res1.text, "html.parser")
-	tags = soup.select("a[href]")
-	print(colored(f"[+] {len(tags)} urls was found in '{url}'", 'green'))
-	print(colored("--------------------------------------", 'blue'))
-	if SingleDomain :
-		for a in tags:
-			alink = a.attrs.get("href")
-			if alink.startswith("/"):
-				urls.append(proto + domain + alink)
-			elif alink.startswith(url) or re.findall(rf".+:\/\/.*{domain}", url) :
-				urls.append(alink)
-	else:
-		for a in tags:
-			alink = a.attrs.get("href")
-			if alink.startswith("/"):
-				urls.append(proto + domain + alink)
-			elif alink.startswith(url) or re.findall(rf".+:\/\/.*{domain}", url) :
-				urls.append(alink)
-			else:
-				urls.append(alink)
+	try:
+		if timeoutValue :
+			res1 = requests.get(url = url, timeout = timeoutValue)
+		else :
+			res1 = requests.get(url)
 
+		if res1.status_code == 200:
+			print(colored(f"[+] {url}: {res1.status_code}", 'green'))
+		else:
+			print(colored(f"[-] {url}: {res1.status_code}", 'red'))
+		print(colored(f"[*] Looking for Urls in {url}", 'yellow'))
+		soup = bs(res1.text, "html.parser")
+		tags = soup.select("a[href]")
+		print(colored(f"[+] {len(tags)} urls was found in '{url}'", 'green'))
+		print(colored("--------------------------------------", 'blue'))
+		if SingleDomain :
+			for a in tags:
+				alink = a.attrs.get("href")
+				if alink.startswith("/"):
+					urls.append(proto + domain + alink)
+				elif alink.startswith(url) or re.findall(rf".+:\/\/.*{domain}", url) :
+					urls.append(alink)
+		else:
+			for a in tags:
+				alink = a.attrs.get("href")
+				if alink.startswith("/"):
+					urls.append(proto + domain + alink)
+				elif alink.startswith(url) or re.findall(rf".+:\/\/.*{domain}", url) :
+					urls.append(alink)
+				else:
+					urls.append(alink)
+
+	except requests.Timeout :
+		print(colored(f"[-] {url}: Timeout"), "red")
 	urls = list(set(urls))
 	urls.remove(url)
 	string = string + url + "\n"
@@ -184,39 +208,6 @@ for url in urls:
 		with open(OutputFileName, "a") as f :
 			f.write(string)
 			string = ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
